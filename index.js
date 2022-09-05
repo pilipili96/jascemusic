c0bd0acc2a984774d00c6a676bf8b8712d6271c1
pilipili96 <pilipili747@gmail.com>
2022-09-04 21:50:37 -0700

    added extra form validation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $('#pressed').on('click', function(){
     let y = document.forms["myForm"]["user_email"].value;
     if (x=="")
         $('#thanks').text('Please fill out your name')
-    else if (y=="")
+    else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(y))
     $('#thanks').text('Please fill out your email')
     else $('#thanks').text('Thank you!')
 })
