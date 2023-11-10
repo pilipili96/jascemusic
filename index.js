c297f3101bf8fd4e39d789cfd82fbad08becd752
pilipili96 <pilipili747@gmail.com>
2023-11-09 23:17:22 -0500

    fixing form

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-$('#pressed').on('click', function(){
-    let x = document.forms["myForm"]["user_name"].value;
-    let y = document.forms["myForm"]["user_email"].value;
-    if (x=="")
-        $('#thanks').text('Please fill out your name')
-    else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(y))
-    $('#thanks').text('Please fill out your email')
-    else $('#thanks').text('Thank you!')
-})
-
-    
-  
-  
-  
-  
-  
-  
-  
+$(document).ready(function() {
+    $('#pressed').on('click', function(){
+        let x = document.forms["myForm"]["user_name"].value;
+        let y = document.forms["myForm"]["user_email"].value;
+        
+        if (x=="")
+            $('#thanks').text('Please fill out your name');
+        else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(y))
+            $('#thanks').text('Please provide a valid email');
+        else
+            $('#thanks').text('Thank you!');
+    });
+});
